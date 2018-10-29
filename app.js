367bd6efea9316e21a49339bde5c0edf14f5e001
Abhiram Madugula <S534093@nwmissouri.edu>
2018-10-28 19:18:11 -0500

    APP JS CHNAGES

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser") // simplifies access to request body
 const fs = require('fs')  // NEW - this is required
 const app = express()  // make express app
 const http = require('http').Server(app)  // inject app into the server
-const port = 8081
+const port = process.env.PORT || 8081;
 
 // ADD THESE COMMENTS AND IMPLEMENTATION HERE 
 // 1 set up the view engine
@@ -77,7 +77,7 @@ app.get(function (req, res) {
 //    })
 
 app.listen(port, function () {
-    console.log('app listening on http://127.0.0.1:' + port)
+    console.log('app listening on ' + port)
   })
  
 
